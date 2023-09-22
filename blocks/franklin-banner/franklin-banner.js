dd517dfc68ba8e8fcd85c317afe834d11a0a8f6c
Sushil Kumar <88363275+kum-sushil@users.noreply.github.com>
2023-09-22 19:09:12 +0530

    commit blocks and css changes

---
+++ blocks/franklin-banner/franklin-banner.js
@@ -0,0 +1,8 @@
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+
+export default function decorate(block) {
+  /* change to ul, li */
+  const banner = document.querySelector('franklin-banner');
+  $(".franklin-banner:nth-child(2)")
+
+}
